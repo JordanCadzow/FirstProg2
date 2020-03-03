28f941e708463444c40dfc26e19ffcb09ae7668a
JORDAN CADZOW <103096811@student.swin.edu.au>
2020-03-03 14:07:51 +1100

    Selection program created

---
+++ IntroProgSelection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntroProgSelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 19;
+            int legalAge = 18;
+            if(age >= legalAge)
+            {
+               Console.WriteLine("You are permitted to get turnt");
+            }
+            else
+            {
+                Console.WriteLine("You are forbidden from drinking");
+            }
+        }
+    }
+}
