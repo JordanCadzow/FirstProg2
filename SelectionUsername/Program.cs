73cc9d9706155b09dd750c8fafb72fe4f3c98aa6
JORDAN CADZOW <103096811@student.swin.edu.au>
2020-03-03 16:36:15 +1100

    Added another selection program

---
+++ SelectionUsername/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionUsername
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sysUsername = "Bob";
+            string sysPassword = "Password";
+
+            Console.WriteLine("Please enter your username");
+            string username = Console.ReadLine();
+            string password = "";
+
+            if(username == sysUsername){
+                Console.WriteLine("Please enter your password");
+                password = Console.ReadLine();
+                if(password == sysPassword){
+                    Console.WriteLine("Login successful");
+                }
+                else{
+                    Console.WriteLine("Incorrect password");
+                }
+            }
+            else{
+                Console.WriteLine("User does not exist");
+            }
+
+
+
+        }
+    }
+}
