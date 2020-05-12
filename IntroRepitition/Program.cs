7370476f748a0eb692e98b3e1691402f8ba5b722
JordanCadzow <103096811@student.swin.edu.au>
2020-05-12 11:50:00 +1000

    created basic repitition program

---
+++ IntroRepitition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntroArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int counter = 0;
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            while (counter < 5)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            Console.WriteLine("Goodbye " + name);
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
 ﻿using System;
 
-namespace FirstProg
+namespace FirstProg2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Hello " + firstName + " " + lastName);
-            Console.WriteLine("What year were you born?");
-            int birthYear = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the current year?");
-            int currentYear = Convert.ToInt32(Console.ReadLine());
-            int age = currentYear - birthYear;
-            Console.WriteLine("So you are " + age + " years old!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
