9e255eb8ffaa590506c4b323ebd7c19496b4991f
JordanCadzow <103096811@student.swin.edu.au>
2020-05-16 18:27:42 +1000

    Completed all classes tasks

---
+++ ClassesTasks/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cookie Cookie1 = new Cookie();
+            Cookie1.Weight = 5;
+            Cookie1.Shape = "Square";
+            Cookie1.colour.Red = 255;
+            Cookie1.colour.Blue = 255;
+            Cookie1.colour.Green = 255;
+            Cookie1.colour.Name = "White";
+
+            Cookie Cookie2 = new Cookie();
+            Cookie2.Weight = 5;
+            Cookie2.Shape = "Circle";
+            Cookie2.colour.Red = 0;
+            Cookie2.colour.Blue = 0;
+            Cookie2.colour.Green = 0;
+            Cookie2.colour.Name = "Black";
+
+            Cookie Cookie3 = new Cookie();
+            Cookie3.Weight = 10;
+            Cookie3.Shape = "Square";
+            Cookie3.colour.Red = 0;
+            Cookie3.colour.Blue = 0;
+            Cookie3.colour.Green = 0;
+            Cookie3.colour.Name = "Black";
+
+            Cookie Cookie4 = new Cookie();
+            Cookie4.Weight = 10;
+            Cookie4.Shape = "Circle";
+            Cookie4.colour.Red = 255;
+            Cookie4.colour.Blue = 255;
+            Cookie4.colour.Green = 255;
+            Cookie4.colour.Name = "White";
+
+
+            Console.WriteLine(Cookie1.Weight);
+            Console.WriteLine(Cookie1.Shape);
+            Console.WriteLine(Cookie1.colour.Name);
+
+        }
+    
+    }
+    public class Cookie {
+        public int Weight;
+        public string Shape;
+        public Colour colour = new Colour();
+
+        // To instantiate a cookie: Cookie (cookiename) = new Cookie();
+    }
+    public class Colour {
+        public int Red;
+        public int Blue;
+        public int Green;
+        public string Name;
+    }
+}
---
+++ ClassesTasks/Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Sam", "Newton", "16/06/1996", 183);
+            Person p2 = new Person("Jordan", "Cadzow", "29/05/2001", 180);
+            Person p3 = new Person("Killmaim", "Deathstab", "1/01/2020", 143);
+
+            Console.WriteLine(p1.GetFullName());
+            Console.WriteLine(p2.GetFullName());
+            Console.WriteLine(p3.GetFullName());
+
+            Console.WriteLine(p1.GetHeightDifference(p2));
+            Console.WriteLine(p1.GetHeightDifference(p3));
+            Console.WriteLine(p2.GetHeightDifference(p3));
+
+            
+        }
+    }
+    
+    public class Person {
+        public string Firstname;
+        public string Surname;
+        public string Dob;
+        public int Height;
+        public int Id;
+        public List<Subject> Subjects;
+
+
+        public Person(string fname, string sname, string dob, int height) {
+            this.Firstname = fname;
+            this.Surname = sname;
+            this.Dob = dob;
+            this.Height = height;
+            Random rand = new Random();
+            this.Id = rand.Next(1000, 9999);
+        }
+
+        public string GetFullName() {
+            return this.Firstname + " " + this.Surname;
+        }
+
+        public int GetHeightDifference(Person other) {
+            if (this.Height > other.Height) {
+                return this.Height - other.Height;
+            }
+            else {
+                return other.Height - this.Height;
+            }
+        }
+
+        public void AddNewSubject(string Name, int YearOfDelivery) {
+            Subject subject = new Subject(Name, YearOfDelivery, "TBD");
+            this.Subjects.Add(subject);
+        }
+
+    }
+    public class Subject {
+        public string Name;
+        public int YearOfDelivery;
+        public string Grade;
+
+        public Subject(string name, int yearofdelivery, string grade) {
+            this.Name = name;
+            this.YearOfDelivery = yearofdelivery;
+            this.Grade = grade;
+        }
+
+    }
+}
